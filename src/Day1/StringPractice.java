c62afe12b24de37b0659149c773281a1dda71f2a
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-02-29 12:38:37 -0600

    my first commit

---
+++ src/Day1/StringPractice.java
@@ -0,0 +1,21 @@
+package Day1;
+
+public class StringPractice {
+    public static void main(String[] args) {
+
+        System.out.println("first day of git and git hub.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
