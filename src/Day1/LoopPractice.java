594d3f5d9c954cb53f3365b4b2cfbc0dd177a48c
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-02-29 20:10:59 -0600

    loop practice

---
+++ src/Day1/LoopPractice.java
@@ -0,0 +1,9 @@
+package Day1;
+
+public class LoopPractice {
+    public static void main(String[] args) {
+        for (int i = 0; i <5; i++) {
+            System.out.println(i);
+        }
+    }
+}
