694274cd66080db40d9baa912052348beb1de89d
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-11 19:17:37 -0500

    new code added

--- src/Day1/Color.java
+++ src/Day1/Color.java
@@ -1,11 +1,20 @@
 package Day1;
 
+import java.util.Arrays;
+
 public class Color {
     public static void main(String[] args) {
 
-        String[] colors={"red","yellow","green"};
+        String[] colors={"red","yellow","green","blue"};
 
 
+        System.out.println(colors.length);
+        System.out.println(Arrays.toString(colors));
+        for (String color:colors
+             ) {
+            System.out.println(color);
+
+        }
 
 
 
