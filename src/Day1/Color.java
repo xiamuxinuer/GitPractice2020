5f32a0a5f35fb21068131df1ff69ffe4bc1e414a
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-11 19:07:06 -0500

    new class added

---
+++ src/Day1/Color.java
@@ -0,0 +1,27 @@
+package Day1;
+
+public class Color {
+    public static void main(String[] args) {
+
+        String[] colors={"red","yellow","green"};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
